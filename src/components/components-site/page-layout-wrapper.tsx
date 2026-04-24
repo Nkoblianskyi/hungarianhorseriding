67d3e29560cce855d9ce9157b608b6a5f86debd8
Nykyta Koblianskyi <nkoblianskyi@gmail.com>
2026-04-24 18:01:41 +0200

    1

--- src/components/components-site/page-layout-wrapper.tsx
+++ src/components/components-site/page-layout-wrapper.tsx
@@ -10,36 +10,24 @@ interface PageLayoutWrapperProps {
 
 export function PageLayoutWrapper({ children }: PageLayoutWrapperProps) {
   return (
-    <html lang="hu">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <body className="antialiased">
-        <div
-          className="min-h-screen relative"
-          style={{
-            backgroundImage: "url(/images/bg-2.jpg)",
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            backgroundAttachment: "fixed",
-            backgroundRepeat: "no-repeat",
-          }}
-        >
-          {/* Dark overlay */}
-          <div className="absolute inset-0 bg-black/40 pointer-events-none"></div>
+    <div
+      className="min-h-screen relative"
+      style={{
+        backgroundImage: "url(/images/bg-2.jpg)",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundAttachment: "fixed",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      {/* Dark overlay */}
+      <div className="absolute inset-0 bg-black/40 pointer-events-none"></div>
 
-          <div className="relative z-10">
-            <MainSiteHeader />
-            <main>{children}</main>
-            <MainSiteFooter />
-          </div>
-        </div>
-      </body>
-    </html>
+      <div className="relative z-10">
+        <MainSiteHeader />
+        <div>{children}</div>
+        <MainSiteFooter />
+      </div>
+    </div>
   )
 }
